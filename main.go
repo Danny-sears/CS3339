69e4b8aa20fc5794dde6ab659d71cfa591ca3e1c
Zimzy <zimzy.buchheit@gmail.com>
2023-10-02 20:39:03 -0500

    Fleshed-out Switch
    Added more cases to the switch statement

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ var txtfile = "rtest1_bin_translated.txt"
 var txtfile_output = ""
 
 const bitlineSize = 32
-const opcodeSize = 11
+const MAXopcodeSize = 11
 
 func main() {
 	//fmt.Println("Hello, World!")
@@ -54,14 +54,34 @@ func formatLine(line string) {
 // This function reads the first 11 bits of each line and uses a switch statement
 // to find the correct opcode
 func defineOpcode(line string) {
-	if len(line) >= opcodeSize {
-		line = line[:opcodeSize]
+	if len(line) >= MAXopcodeSize {
+		line = line[:MAXopcodeSize]
 
 		// This switch statement finds the opcode instruction
 		switch line {
+
+		// Checks 11-bit opcode cases FIRST, then narrows down to 9, 8, and 6-bit opcodes
+		case "10001010000":
+			fmt.Println("AND")
 		case "10001011000":
 			fmt.Println("ADD") // Currently for testing
-		}
+		case "10101010000":
+			fmt.Println("ORR")
+		case "11001011000":
+			fmt.Println("SUB")
+		case "11010011010":
+			fmt.Println("LSR")
+		case "11010011011":
+			fmt.Println("LSL")
+		case "11111000000":
+			fmt.Println("STUR")
+		case "11111000010":
+			fmt.Println("LDUR")
+		case "11010011100":
+			fmt.Println("ASR")
+		case "11101010000":
+			fmt.Println("EOR")
+		} // STILL NEEDS THE REST OF THE OPCODES
 		fmt.Println(line) // Currently for testing
 	}
 
